0e24fb146db88786865db0132cae42a9d2aad9e6
Matteo Arcangeli <matteoa1999@gmail.com>
2022-10-01 17:24:24 +0200

    Upload script

---
+++ data/unifyjson.py
@@ -0,0 +1,34 @@
+# Script that unify json from app store and play store removing the unused fields
+
+import json
+
+
+class Review:
+    def __init__(self, data, review, rating):
+        self.data = data
+        self.review = review
+        self.rating = rating
+
+list = []
+
+with open('app_store.json', 'r') as appstore_json_file:
+    appstore_json_object = json.load(appstore_json_file)
+for obj in appstore_json_object:
+    list.append(dict(date=obj["date"],
+                     review=obj["review"],
+                     rating=obj["rating"],
+                     platform="ios"))
+
+with open('play_store.json', 'r') as appstore_json_file:
+    appstore_json_object = json.load(appstore_json_file)
+for obj in appstore_json_object:
+    list.append(dict(date=obj["at"],
+                     review=obj["content"],
+                     rating=obj["score"],
+                     platform="android"))
+
+json_str = json.dumps(list)
+
+text_file = open("data.json", "w")
+text_file.write(json_str)
+text_file.close()

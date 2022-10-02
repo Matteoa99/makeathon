5fe1e5dd76717e940926d3d634fa2fa97839119c
Matteo Arcangeli <matteoa1999@gmail.com>
2022-10-02 10:00:33 +0200

    Update

--- data/unifyjson.py
+++ data/unifyjson.py
@@ -4,27 +4,37 @@ import json
 
 
 class Review:
-    def __init__(self, data, review, rating):
+    def __init__(self,id, data, review, rating, platform):
+        self.id = id
         self.data = data
         self.review = review
         self.rating = rating
+        self.platform = platform
 
 list = []
 
 with open('app_store.json', 'r') as appstore_json_file:
     appstore_json_object = json.load(appstore_json_file)
 for obj in appstore_json_object:
-    if obj["review"] == "":
-        list.append(dict(date=obj["date"], review=obj["review"], rating=obj["rating"], platform="ios"))
+    list.append(dict(
+                     id=obj['userName'],
+                     date=obj["date"],
+                     review=obj["review"],
+                     rating=obj["rating"],
+                     platform="ios"))
 
 with open('play_store.json', 'r') as appstore_json_file:
     appstore_json_object = json.load(appstore_json_file)
 for obj in appstore_json_object:
-    if obj["content"] == "":
-        list.append(dict(date=obj["at"], review=obj["content"],  rating=obj["score"], platform="android"))
+    list.append(dict(
+                     id=obj['reviewId'],
+                     date=obj["at"],
+                     review=obj["content"],
+                     rating=obj["score"],
+                     platform="android"))
 
 json_str = json.dumps(list)
 
-text_file = open("data.json", "w")
+text_file = open("unified_review.json", "w")
 text_file.write(json_str)
 text_file.close()
